0aa8e5c059572f9da6475f8c6f17aeea48db6db3
LukrowanyLouie <seventyy@o2.pl>
2025-01-21 20:36:41 +0100

    Initial commit

---
+++ src/test/java/pl/edu/vistula/Zadanie1/Zadanie1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.Zadanie1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
